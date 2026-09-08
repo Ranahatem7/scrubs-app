31b9a81ad4459b5708448c7172fc8c27a54e1b43
kanzywaleed13 <kanzy.waleed113@gmail.com>
2026-09-09 00:46:42 +0300

    Update orders.js

--- frontend/src/services/orders.js
+++ frontend/src/services/orders.js
@@ -1,7 +1,7 @@
 import { api } from "./api";
 
-export function createOrder({ items, shipping, paymentMethod }) {
-  return api.post("/orders", { items, shipping, paymentMethod });
+export function createOrder({ items, shipping, paymentMethod, shippingFee, discountAmount, discountCode }) {
+  return api.post("/orders", { items, shipping, paymentMethod, shippingFee, discountAmount, discountCode });
 }
 
 export function getMyOrders() {
